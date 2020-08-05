623b8fe2cf1dc1917d0118f69a8aa8b8924a0b2e
IRWAN BIN IBRAHIM <irwanibrahim@commercedc.com.my>
2020-08-05 10:41:22 +0800

    dev

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/admin/');
+define('DIR_EXTENSION', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/extension/');
+define('DIR_IMAGE', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/image/');
+define('DIR_SYSTEM', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/system/');
+define('DIR_STORAGE', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/storage/');
+define('DIR_CATALOG', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'pdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'iwang');
+define('DB_PASSWORD', 'Root@!234');
+define('DB_DATABASE', 'spe_ecommerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'spe_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/catalog/');
+define('DIR_EXTENSION', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/extension/');
+define('DIR_IMAGE', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/image/');
+define('DIR_SYSTEM', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/upload/system/');
+define('DIR_STORAGE', 'D:/ExternalCode/Onewoorks/sistem-pengurusan-emas/ecommerce/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'pdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'iwang');
+define('DB_PASSWORD', 'Root@!234');
+define('DB_DATABASE', 'spe_ecommerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'spe_');
